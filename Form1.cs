5544e1708825939f761f2a0141ad7fcf349f10c2
bao1310zz <you@example.com>
2025-10-02 22:04:10 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.toolStripStatusLabel1.Text = string.Format("Hôm  nay  là  ngày  {0}  -  Bây  giờ  là {1}", DateTime.Now.ToString("dd/MM/yyyy"), DateTime.Now.ToString("hh:mm:ss tt"));
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         OpenFileDialog dlg = new OpenFileDialog();
+         dlg.Filter = "AVI file| *.avi | MPEG File | *.mpeg | Wav File |  *.Wav | Midi File | *.midi | Mp4 File | *.mp4 | MP3 | *.mp3";
+             if (dlg.ShowDialog() == DialogResult.OK)
+                axWindowsMediaPlayer1.URL = dlg.FileName;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
